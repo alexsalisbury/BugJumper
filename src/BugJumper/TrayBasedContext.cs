ebe4f1657cb32229bc9ffefd3bb05c24f47fa12c
Alex Salisbury <alex@alexsalisbury.com>
2019-06-02 15:02:23 -0400

    add Initial System Tray work. AB#274

---
+++ src/BugJumper/TrayBasedContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BugJumper
+{
+    public interface IIconInfo
+    {
+        Icon Icon { get;}
+    }
+
+    public class TrayBasedContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public TrayBasedContext(IIconInfo appIcon)
+        {
+            var exit = new MenuItem("Exit", Exit);
+
+            MenuItem[] main = { exit };
+
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = appIcon?.Icon,
+                ContextMenu = new ContextMenu(main),
+                Visible = true
+            };
+        }
+
+        public void Exit(object sender, EventArgs e)
+        {
+        }
+    }
+}
