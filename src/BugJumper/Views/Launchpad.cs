637254e25a8e104fdf42c6b65009673c1ec455e3
Alex Salisbury <alex@alexsalisbury.com>
2019-06-23 11:52:27 -0400

    Add Launchpad form AB#285

---
+++ src/BugJumper/Views/Launchpad.cs
@@ -0,0 +1,22 @@
+﻿namespace BugJumper.Views
+{
+    using System;
+    using System.Windows.Forms;
+    using BugJumper;
+
+    public partial class Launchpad : Form
+    {
+        public Launchpad(string urlFormat)
+        {
+            InitializeComponent();
+            if (string.IsNullOrWhiteSpace(urlFormat))
+            {
+                this.Close();
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
