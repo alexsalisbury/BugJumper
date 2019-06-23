1a78235f8fb71dc844e5fe91be193e905869cd5f
Alex Salisbury <alex@alexsalisbury.com>
2019-06-23 11:48:40 -0400

    Move EventArgs to Events folder

---
+++ src/BugJumper/Events/UrlFormatChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace BugJumper
+{
+    using System.ComponentModel;
+
+    public class UrlFormatChangedEventArgs : HandledEventArgs
+    {
+        public string UriFormat { get; private set; }
+
+        public UrlFormatChangedEventArgs(string format)
+        {
+            this.UriFormat = format;
+        }
+    }
+}
