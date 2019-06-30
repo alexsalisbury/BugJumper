7d810df43cb79409f7a0e7cf08af9d926129c52c
Alex Salisbury <alex@alexsalisbury.com>
2019-06-30 08:23:52 -0400

    UrlLauncher class creation. AB#290

---
+++ src/BugJumper/UrlLauncher.cs
@@ -0,0 +1,29 @@
+﻿namespace BugJumper
+{
+    using System;
+
+    public interface IUrlLauncher
+    {
+        void Launch(string url);
+    }
+
+    //http://faithlife.codes/blog/2008/01/using_processstart_to_link_to/
+    public class UrlLauncher : IUrlLauncher
+    {
+        public void Launch(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch (Exception)
+            {
+                // This catch should only occur if the user doesn't have a default browser for some reason.
+                Process.Start("iexplore.exe", url);
+            }
+            finally
+            {
+            }
+        }
+    }
+}
