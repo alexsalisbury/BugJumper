5012d66bc3a9a98225aae9da728c989b157774e6
Alex Salisbury <alex@alexsalisbury.com>
2019-06-30 08:37:46 -0400

    Wire up launcher to Go button. AB#287

--- src/BugJumper/Views/Launchpad.cs
+++ src/BugJumper/Views/Launchpad.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Windows.Forms;
     using BugJumper;
+    using BugJumper.Services;
 
     public interface INamedForm
     {
@@ -11,24 +12,42 @@
 
     public partial class Launchpad : Form, INamedForm
     {
-        string urlFormat;
-
         public static string CanonicalName => "LAUNCHPAD";
-
         public event EventHandler<UrlFormatChangedEventArgs> UrlFormatChanged;
 
-        public Launchpad(string urlFormat)
+        private string urlFormat;
+        private IUrlLauncher launcher;
+
+        public Launchpad(IUrlLauncher launcher, string urlFormat)
         {
             InitializeComponent();
-            UrlFormatChanged += Launchpad_UrlFormatChanged;
+            
             if (string.IsNullOrWhiteSpace(urlFormat))
             {
                 this.Close();
             }
+
+            UrlFormatChanged += Launchpad_UrlFormatChanged;
+            this.launcher = launcher;
         }
 
         private void btnGo_Click(object sender, EventArgs e)
         {
+            string target = string.Empty;
+
+            if (int.TryParse(mtxtNumber.Text, out int value))
+            {
+                target = string.Format(urlFormat, value.ToString());
+            }
+            //else
+            //{
+            //    //Check if entered value is a known keyword and launch that url instead.
+            //}
+
+            if(!string.IsNullOrWhiteSpace(target))
+            {
+                launcher.Launch(target);
+            }
         }
 
         private void Launchpad_UrlFormatChanged(object sender, UrlFormatChangedEventArgs e)
